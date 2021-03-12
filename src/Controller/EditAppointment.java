0733dd58a30fd7bb7e0989ea2d0b93fcf02cdaaf
curiouscoding22 <lambertbe22@gmail.com>
2021-03-11 19:58:42 -0500

    Retrieve appointment method complete

--- src/Controller/EditAppointment.java
+++ src/Controller/EditAppointment.java
@@ -14,6 +14,8 @@ import utils.DBQuery;
 
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
 
 public class EditAppointment implements Initializable {
@@ -34,6 +36,7 @@ public class EditAppointment implements Initializable {
     @FXML private ComboBox customerCombo;
     @FXML private Button cancelButton;
 
+    private Appointment appointment;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -72,6 +75,66 @@ public class EditAppointment implements Initializable {
     }
 
     public void retrieveSelectAppointment(Appointment appointment) {
+        this.appointment = appointment;
+        IDField.setText(Integer.toString(appointment.getAppointmentID()));
+        titleField.setText(appointment.getTitle());
+        descriptionField.setText(appointment.getDescription());
+        locationField.setText(appointment.getLocation());
+        for(Contact con: Contact.contacts){
+            if(appointment.getContact().equals(con.getContactName())){
+                contactCombo.setValue(con);
+            }
+        }
+        typeField.setText(appointment.getType());
+        dateSelector.setValue(appointment.getStart().toLocalDate());
+        LocalDateTime start = appointment.getStart();
+        if(start.getHour() == 12){
+            startHRCombo.setValue(start.getHour());
+            startAMPM.setValue("PM");
+        } else if(start.getHour() > 12){
+            startHRCombo.setValue(start.getHour() - 12);
+            startAMPM.setValue("PM");
+        } else {
+            startHRCombo.setValue(start.getHour());
+            startAMPM.setValue("AM");
+        }
+        if(start.getMinute() == 0){
+            startMinCombo.setValue("OO");
+        } else if(start.getMinute() == 15){
+            startMinCombo.setValue("15");
+        } else if(start.getMinute() == 30){
+            startMinCombo.setValue("30");
+        } else if(start.getMinute() == 45){
+            startMinCombo.setValue("45");
+        }
+        LocalDateTime end = appointment.getEnd();
+        if(end.getHour() == 12){
+            endHRCombo.setValue(end.getHour());
+            endAMPM.setValue("PM");
+        } else if(end.getHour() > 12){
+            endHRCombo.setValue(end.getHour() - 12);
+            endAMPM.setValue("PM");
+        } else {
+            endHRCombo.setValue(end.getHour());
+            endAMPM.setValue("AM");
+        }
+        if(end.getMinute() == 0){
+            endMinCombo.setValue("OO");
+        } else if(end.getMinute() == 15){
+            endMinCombo.setValue("15");
+        } else if(end.getMinute() == 30){
+            endMinCombo.setValue("30");
+        } else if(end.getMinute() == 45){
+            endMinCombo.setValue("45");
+        }
+
+        for(Customer cus : Customer.customers){
+            if(appointment.getCustomerID() == cus.getCustomerID()){
+                customerCombo.setValue(cus);
+            }
+        }
+
+
     }
 
     public void saveUpdatedAppointment(ActionEvent actionEvent) {
@@ -88,4 +151,6 @@ public class EditAppointment implements Initializable {
             }
         });
     }
+
+
 }
