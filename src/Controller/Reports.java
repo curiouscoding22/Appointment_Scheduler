6626feeb385e755065410f5e5c1fe879d494b891
curiouscoding22 <lambertbe22@gmail.com>
2021-03-20 19:22:06 -0400

    Added report GUI and controller. Renamed util file.

--- src/Controller/Reports.java
+++ src/Controller/Reports.java
@@ -1,4 +1,28 @@
 package Controller;
 
-public class Reports {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Reports implements Initializable {
+
+    @FXML private TextField reportTextArea;
+
+    public void runReportOne(ActionEvent actionEvent) {
+    }
+
+    public void runReportTwo(ActionEvent actionEvent) {
+    }
+
+    public void runReportThree(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
